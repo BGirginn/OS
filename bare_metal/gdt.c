e470e567a7f82dd9cd558a90d810a87c1b51969d
BGirgin <girginbora30@gmail.com>
2026-04-25 17:12:42 +0300

    gdt_flush

--- bare_metal/gdt.c
+++ bare_metal/gdt.c
@@ -3,6 +3,12 @@ GDT Kaydı (Entry): Her bir hafıza diliminin (Kod, Veri vb.) tanımı.
 GDT İşaretçisi (Pointer): İşlemciye "GDT tablom tam olarak şurada" diyen özel bir yapı.
 Assembly Yükleyici: İşlemciye bu yeni tabloyu kullanmasını emreden küçük bir kod. */
 
+struct gdt_entry gdt[3];  /* buradaki üç 0 1 2 den gelen 3 adet anlamına geliyor. 3 adet gdt_entry oluşturuyor 3 farklı gdt var.
+gdt[0] = boş segment burası hep boş kalır.
+gtd[1] = kod kısmı burası sadece okunabilir. 
+gdt[2] = veri kısmı okunabilir yazılabilir. */
+
+/* Sen fonksiyonun içinde gdt[num].limit_low yazdığında, işlemci ye num kutusunun içine hangi sayı gelirse, git o numaralı kata ve oradaki limit_low çekmecesini aç komutu verir */
 
 /* base bizim ram bölgemizin başlangıcı limit ise ram bölgemizin ne kadar olacağıdır */
 struct gdt_entry {
@@ -15,3 +21,53 @@ struct gdt_entry {
 } __attribute__((packed));
 /*__attribute__((packed)) demezsen, derleyici (GCC) hızı artırmak için bu değişkenlerin arasına gizli boşluklar koyabilir.
 Ama işlemci bu veriyi tam olarak 8 bayt (boşluksuz) bekler. Bu yüzden "paketle" diyoruz.*/
+
+void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran) 
+{
+    
+    /* 1. Base (Adres) Parçalama: 32 bitlik adresi 3 çekmeceye bölüyoruz */
+    gdt[num].base_low    = (base & 0xFFFF);        // İlk 16 bit (Maske ile kes-al)
+    gdt[num].base_middle = (base >> 16) & 0xFF;    // Orta 8 bit (16 it, sonra kes-al)
+    gdt[num].base_high   = (base >> 24) & 0xFF;    // Son 8 bit (24 it, sonra kes-al)
+
+    /* 2. Limit (Boyut) Parçalama: Toplam 20 bitlik limitin ilk 16'sı buraya */
+    gdt[num].limit_low   = (limit & 0xFFFF);
+
+    /* 3. Granularity: Hem limitin kalan 4 bitini hem de ayarları buraya sıkıştırıyoruz */
+    gdt[num].granularity = ((limit >> 16) & 0x0F); // Limitin son 4 bitini aldık
+    gdt[num].granularity |= (gran & 0xF0);         // Üstüne gran ayarlarını "OR" ile yapıştırdık
+
+    /* 4. Erişim Hakları */
+    gdt[num].access      = access;
+}
+
+struct gdt_ptr {
+    unsigned short limit; // Tablonun toplam boyutu (Bayt cinsinden - 1). Bu yukardaki limitten farklı buradaki boyut tutuyor yukardaki ise ilk 16 biti.
+    unsigned int   base;  // Tablonun başladığı tam adres
+} __attribute__((packed));
+
+struct gdt_ptr gp; // Bu bizim işlemciye vereceğimiz "kartvizit" olacak. structtan eleman üretmek için bu gp yerine gp1 dersen gp1.limit, gp2.limit gibi birden fazla üretebilirsin.    
+
+void gdt_install() {
+    /* 1. GDT İşaretçisini (gp) hazırlayalım */
+    gp.limit = (sizeof(struct gdt_entry) * 3) - 1;
+    gp.base  = (unsigned int)&gdt;
+
+    /* 2. Kapıları tek tek inşa edelim */
+    
+    // 0. Kapı: Boş (Zorunlu)
+    gdt_set_gate(0, 0, 0, 0, 0);
+
+    // 1. Kapı: Kod Segmenti (0'dan başla, 4GB, Kod erişimi, 32-bit sayfalama)
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+
+    // 2. Kapı: Veri Segmenti (0'dan başla, 4GB, Veri erişimi, 32-bit sayfalama)
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+    /* standardı bu şekilde ondan bir nevi sabit bu */
+
+    /* 3. ŞİMDİ NE OLACAK? */
+    // Buraya kadar her şey C dilindeydi. 
+    // Ama işlemciye "Bu GDT'yi yükle" demek için 
+    // Assembly dilinde 'lgdt' komutunu kullanmamız gerekecek.
+}
---
+++ bare_metal/gdt_flush.s
@@ -0,0 +1,23 @@
+# Eski (GRUB'dan kalan) ayarları siler ve senin ayarlarını işlemcinin her hücresine yayar.
+
+.global gdt_flush    # C kodundan bu fonksiyona ulaşabilmek için sessionu global yaptık
+.extern gp           # C'de tanımladığımız 'gp' (kartvizit) burada kullanacağız. başka bir dosyada gp diye değişken tanımladım bu o değişken demek. 
+/* Başka bir gp tanımlı olamaz mesela başka yerde bu bir tür hata olur o yüzden nereden bulacak diye merak etme */
+
+gdt_flush:
+    lgdt (gp)        # 1. ADIM: İşlemciye GDT'nin yerini (gp) bildir. Git bu gp ismindeki değişkenin olduğu adresteki veriyi (6 baytlık kartviziti) oku" der
+    # Senin RAM'de hazırladığın gp yapısındaki adresi ve limiti alır, işlemcinin içine kilitler. Artık işlemci nereye bakacağını bilir.
+
+    mov $0x10, %ax   # 0x demek 16 bitlik demek 0*8 gdt0, 1*8 gdt1 2*8 gdt 2 burada da 0x10 demek 16 demek ve ax i gdt2 ye yolladık ax genel amaçla kullandığımız bir register türü 
+    mov %ax, %ds     # buradaki ds es fs gs ss leri direkt ax e yollayarak gdt2 ye gönderiyoruz direkt gdt2 ye yollayamayız çalışmaz. ds (Data Segment): Normal değişkenler için.
+    mov %ax, %es     # es, fs, gs: Ekstra veri alanları için.
+    mov %ax, %fs
+    mov %ax, %gs
+    mov %ax, %ss     # ss (Stack Segment): Fonksiyonların dönüş adresleri ve yerel değişkenler (Yığın) için.
+
+    # 3. ADIM: Kodlarımızın bulunduğu segmenti (0x08) aktif etmek için "Uzak Zıplama" yap.
+    # Bu satır işlemcinin içindeki CS kaydını 0x08 (1 * 8) yapar.
+    ljmp $0x08, $.flush  # burada sistemi okunabilir ve yazılabilir alandan sadece okunabilir kural sistemine geçirip sonra da .flush kodunu sıraya alıyorsun
+
+.flush:
+    ret              # C koduna geri dön!
